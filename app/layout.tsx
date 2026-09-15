8842f63cd923c28664c5eddd7930a49b3ef002a5
shwl3030 <shwl3030@yahoo.com>
2026-09-15 20:08:10 +0800

    Add koi fish brand mark

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
     "codex-preview": "development",
   },
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: "/koi-logo.png",
+    shortcut: "/koi-logo.png",
   },
 };
 
